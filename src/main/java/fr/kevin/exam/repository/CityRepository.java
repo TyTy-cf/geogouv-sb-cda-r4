bc1ca4fb7b8cb5b66b7bce86f4ad8b3ba132c215
Kevin Tourret <kevin@drosalys.fr>
2024-10-10 17:04:00 +0200

    add custom query

--- src/main/java/fr/kevin/exam/repository/CityRepository.java
+++ src/main/java/fr/kevin/exam/repository/CityRepository.java
@@ -1,10 +1,16 @@
 package fr.kevin.exam.repository;
 
 import fr.kevin.exam.entity.City;
+import fr.kevin.exam.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    List<City> findTop10ByOrderByPopulationDesc();
 
 }
